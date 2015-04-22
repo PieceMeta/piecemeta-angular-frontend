9fdb5f29de4a4b3657e40f0c75dfd35b249a1a5a
Anton Koch <anton.koch@gmail.com>
2015-04-22 15:04:01 +0200

    restored settings...

--- dist/web/js/piecemeta-angular-frontend.js
+++ dist/web/js/piecemeta-angular-frontend.js
@@ -9,8 +9,8 @@ var PIECEMETA_DEV_API_URL = 'https://api.piecemeta.com';
 var PIECEMETA_API_HOST = 'https://api.piecemeta.com';
 
 // DEV
-PIECEMETA_DEV_API_URL = 'http://localhost:8080';
-PIECEMETA_API_HOST = 'http://localhost:8080';
+//PIECEMETA_DEV_API_URL = 'http://localhost:8080';
+//PIECEMETA_API_HOST = 'http://localhost:8080';
 /* global angular,console */
 (function () {
     'use strict';
